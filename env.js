f39d819018435e44975a98a00aef9aff6423ffc1
Mykolas Krupauskas <mkrupauskas@gmail.com>
2020-02-11 20:39:18 +0100

    requires

--- env.js
+++ env.js
@@ -1,2 +1,3 @@
-export const DB_URL =
-    'mongodb://mykolas:mykolas1@ds123136.mlab.com:23136/foodie';
+const DB_URL = 'mongodb://mykolas:mykolas1@ds123136.mlab.com:23136/foodie';
+
+exports.DB_URL = DB_URL;
--- functions/get.js
+++ functions/get.js
@@ -1,6 +1,6 @@
-import mongoose from 'mongoose';
+const mongoose = require('mongoose');
 
-import { DB_URL } from './../env';
+const { DB_URL } = require('./../env');
 
 exports.handler = async (event, context) => {
     const db = await mongoose.connect(DB_URL);
