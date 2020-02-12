b00f7afd68e6febc9edaa10bb8a031e00718e5c9
Mykolas Krupauskas <mkrupauskas@gmail.com>
2020-02-12 10:06:57 +0100

    add database endpoint

--- functions/endpoint.js
+++ functions/endpoint.js
@@ -1,7 +1,27 @@
-exports.handler = (event, context, callback) => {
-    const response = {
+const mongoose = require('mongoose');
+
+const { DB_URL } = require('./env');
+
+let db;
+
+const reviewsSchema = new mongoose.Schema({
+    created: Date,
+    text: String,
+    rating: Number,
+});
+
+exports.handler = async (event, context) => {
+    if (!db) {
+        db = await mongoose.connect(DB_URL);
+        db.model('reviews', reviewsSchema);
+    }
+
+    const reviewsModel = db.model('reviews');
+
+    const reviews = await reviewsModel.find().exec();
+
+    return {
         statusCode: 200,
-        body: "Hi, let's go Serverless!! 🚀🚀🚀🌞",
+        body: JSON.stringify(reviews),
     };
-    callback(null, response);
 };
