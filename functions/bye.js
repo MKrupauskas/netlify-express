6989de75157f01db3eef7807302658915cb663d4
Mykolas Krupauskas <mkrupauskas@gmail.com>
2020-02-12 10:16:05 +0100

    prepare for demo

--- functions/bye.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async (event, context) => {
-    console.log({ event, context });
-
-    return {
-        statusCode: 400,
-        body: 'Hello, sorry about that',
-    };
-};
--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,6 @@
 exports.handler = async (event, context) => {
     return {
-        statusCode: 400,
-        body: 'Hello, sorry about that',
+        statusCode: 200,
+        body: 'Hello world',
     };
 };
