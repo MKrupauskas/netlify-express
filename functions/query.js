0a4c119255e869b573925630ef9b0612e445344a
Mykolas Krupauskas <mkrupauskas@gmail.com>
2020-02-12 11:48:23 +0100

    log event object

---
+++ functions/query.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const { DB_URL } = require('./env');
+
+const reviewsSchema = new mongoose.Schema({
+    created: Date,
+    text: String,
+    rating: Number,
+});
+
+let db = null;
+
+exports.handler = async (event, context) => {
+    if (!db) {
+        db = await mongoose.connect(DB_URL);
+        db.model('reviews', reviewsSchema);
+    }
+
+    console.log(event);
+
+    const reviewsModel = db.model('reviews');
+
+    const reviews = await reviewsModel.find().exec();
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(reviews),
+    };
+};
--- todo.md
+++ todo.md
@@ -1,3 +1,4 @@
 1. new simple lamdba endpoint
 2. async syntactic sugar
-3. 
+3. new API endpoint for db review data
+4. client application with API
